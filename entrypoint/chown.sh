446bedd1a548641a0f4dd877a48fe78230652bb1
kolserdav <serega12101983@gmail.com>
2025-02-10 13:28:50 +0700

    fix

--- entrypoint/chown.sh
+++ entrypoint/chown.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/bash
 
-#! /usr/bin/bash
+set -e
 
-ftp_user=www-data
+fpm_user=www-data
 upload_dir=wp-content/uploads
 
 echo "Change dir ($app_dir) owner: $fpm_user"
 
 mkdir -p "$upload_dir"
-chown "$ftp_user":"$ftp_user" -R "$upload_dir"
+chown "$fpm_user":"$fpm_user" -R "$upload_dir"
 
 # Run default entrypoint
 exec "$@"
