cd10b08130a6769bc95a8fba5a10de11a4a78452
kolserdav <serega12101983@gmail.com>
2025-02-10 13:05:50 +0700

    fixed chown

--- entrypoint/chown.sh
+++ entrypoint/chown.sh
@@ -1,13 +1,19 @@
 #! /usr/bin/bash
 
+set -e
+
 fpm_user=www-data
 app_dir="."
-git=.git
+git_dir=.git
 
-echo "Change app dir ($app_dir) owner: $fpm_user, except dir $git"
+echo "Change app dir ($app_dir) owner: $fpm_user, except dir $gigit_dirt"
+
+git_owner=$(stat -c "%U:%G" "$app_dir/$git_dir")
 
 mkdir -p "$app_dir"
-find "$app_dir" -path "$app_dir"/$git -prune -o -exec chown "$fpm_user":"$fpm_user" -R {} +
+chown "$fpm_user":"$fpm_user" -R $app_dir
+# Chown git owner back, else git runner won't work
+chown "$git_owner":"$git_owner" -R "$app_dir/$git_dir"
 
 # Run default entrypoint
 exec "$@"
